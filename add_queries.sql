42bbaa036102002096bf392247bc521e4ff3c706
brytebee <brytebee@gmail.com>
2022-03-22 10:45:40 +0100

    :white_check_mark: Update query
    Add query for all animals that are neutered

--- add_queries.sql
+++ add_queries.sql
@@ -1,3 +1,3 @@
-SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = true;
 -- SELECT * FROM animals;
 -- SELECT count(*) AS exact_count FROM animals;
