e3d0eab4c08fc0fb434d7c89872a465a5055afed
brytebee <brytebee@gmail.com>
2022-03-22 13:22:04 +0100

    :white_check_mark: Alter table schema
    Modified the table schema to include a species column of String data type

--- queries.sql
+++ queries.sql
@@ -6,3 +6,6 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+-- Modify/Alter/Update Table
+ALTER TABLE animals ADD COLUMN species TYPE VARCHAR(150);
