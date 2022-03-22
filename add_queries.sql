bcc1a3f69bc88f0096888a5f30e4485a8e4e827e
brytebee <brytebee@gmail.com>
2022-03-22 10:55:08 +0100

    :white_check_mark: Update query
    Add query for all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)

--- add_queries.sql
+++ add_queries.sql
@@ -1,3 +1,3 @@
-SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 -- SELECT * FROM animals;
 -- SELECT count(*) AS exact_count FROM animals;
