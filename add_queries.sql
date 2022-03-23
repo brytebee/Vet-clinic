d0cf360634e87871ddc3b29290b9069d39abc130
brytebee <brytebee@gmail.com>
2022-03-23 11:48:16 +0100

    :white_check_mark: Insert data into owners' table
    Inserted data of owners

---
+++ add_queries.sql
@@ -0,0 +1,6 @@
+INSERT INTO owners (name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (name, age) VALUES ('Bob', 45);
+INSERT INTO owners (name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (name, age) VALUES ('Jodie Whittaker', 38);
