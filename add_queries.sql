b89fea0d85761ea8f1b8f2571d8ab5ccf590112d
brytebee <brytebee@gmail.com>
2022-03-23 11:36:36 +0100

    :white_check_mark: Add owners_id column to animals table
    Added owners_id column to the animals table to Ref the owners' table

--- add_queries.sql
+++ add_queries.sql
@@ -11,3 +11,4 @@ CREATE TABLE species (
 
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
