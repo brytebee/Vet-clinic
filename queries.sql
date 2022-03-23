281ece93b0c76fdfdf3a27fedcfa62a6c2557bd0
brytebee <brytebee@gmail.com>
2022-03-23 18:54:08 +0100

    :white_check_mark: Add animals for Melody Pond
    Added animals belonging Melody Pond

--- queries.sql
+++ queries.sql
@@ -49,3 +49,10 @@ SELECT ROUND(AVG(weight_kg)::numeric, 2) FROM animals; --Round to 2 decimal plac
 SELECT name, species, escape_attempts FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
 SELECT species, MIN(weight_kg) min_weight, MAX(weight_kg) max_weight FROM animals GROUP BY species;
 SELECT species,  ROUND(AVG(escape_attempts)::NUMERIC, 0) Esc_try FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species; --approx to whole number
+
+-- Queries for 3rd milestone
+SELECT full_name, name
+FROM (
+  SELECT name, full_name FROM animals JOIN owners WHERE animals.owner_id = owners.id
+) _
+WHERE full_name = 'Melody Pond';
