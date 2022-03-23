98bda98447bb42d7fd8d444d64768f45b1e63423
brytebee <brytebee@gmail.com>
2022-03-23 11:53:55 +0100

    :fire: Cleanup
    Moved data for owners' into data.sql

--- add_queries.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO owners (name, age) VALUES ('Sam Smith', 34);
-INSERT INTO owners (name, age) VALUES ('Jennifer Orwell', 19);
-INSERT INTO owners (name, age) VALUES ('Bob', 45);
-INSERT INTO owners (name, age) VALUES ('Melody Pond', 77);
-INSERT INTO owners (name, age) VALUES ('Dean Winchester', 14);
-INSERT INTO owners (name, age) VALUES ('Jodie Whittaker', 38);
