fdada1e816655ca6fb24bfcfbbc49c9c61a817e1
brytebee <brytebee@gmail.com>
2022-03-24 14:32:41 +0100

    :white_check_mark: Update query
    Query for how many visits were with a vet that did not specialize in that animal's species

--- queries.sql
+++ queries.sql
@@ -75,3 +75,4 @@ SELECT name, COUNT(date_of_birth) FROM animals a JOIN visits v ON a.id = v.anima
 SELECT a.name, vt.name, date_of_visit FROM visits v JOIN vets vt ON v.vets_id = vt.id JOIN animals a ON v.animals_id = a.id WHERE vt.name = 'Maisy Smith' ORDER BY date_of_visit LIMIT 1;
 SELECT a.name AS ani_name, vt.name AS vet_name, date_of_visit FROM visits v JOIN animals a ON v.animals_id = a.id JOIN vets vt ON v.vets_id = vt.id ORDER BY date_of_visit DESC LIMIT 1;
 SELECT COUNT(sp.name), sp.name FROM visits v JOIN animals a ON v.animals_id = a.id JOIN species sp ON a.species_id = sp.id JOIN vets vt ON v.vets_id = vt.id WHERE vets_id = 2 GROUP BY(sp.name) limit 1;
+SELECT COUNT(*) AS species FROM visits v FULL OUTER JOIN vets vt ON v.vets_id = vt.id FULL OUTER JOIN specializations s ON s.vets_id = vt.id WHERE species_id IS NULL;
